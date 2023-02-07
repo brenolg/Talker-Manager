2a30ca9c6c82b623583cd96b488b987f3a82a487
Breno Lavalle Garrido <brenolg@yahoo.com.br>
2023-02-07 15:03:53 -0300

    feat:2 - Crie o endpoint GET /talker/:id

--- src/routes/talkerRoute.js
+++ src/routes/talkerRoute.js
@@ -9,4 +9,12 @@ routerTalker.get('/', async (_req, res) => {
      return res.status(200).json(talkers); 
 });
 
+routerTalker.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const talkers = await readTalker();
+    const findId = talkers.find((talker) => talker.id === Number(id));
+    if (!findId) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    return res.status(200).json(findId);
+});
+
 module.exports = routerTalker;
