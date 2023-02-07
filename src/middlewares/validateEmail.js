db43d9a11047c9f6cf08d41ecc045682b0bdfe2f
Breno Lavalle Garrido <brenolg@yahoo.com.br>
2023-02-07 17:10:47 -0300

    feat: 4 - Adicione as validações para o endpoint /login

---
+++ src/middlewares/validateEmail.js
@@ -0,0 +1,12 @@
+const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/i;
+
+const validateEmail = (req, res, next) => {
+    const { email } = req.body;
+    if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' }); 
+    if (!regex.test(email)) {
+        return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+    }
+     return next();
+};
+
+module.exports = validateEmail;
--- src/routes/loginRoute.js
+++ src/routes/loginRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const crypto = require('crypto');
+const validateEmail = require('../middlewares/validateEmail');
+const validatePassword = require('../middlewares/validatePassword');
 
 const routerLogin = express.Router();
 
-routerLogin.post('/', async (req, res) => {
-    console.log('body', req.body);
+routerLogin.post('/', validatePassword, validateEmail, async (_req, res) => {
     const token = crypto.randomBytes(8).toString('hex');
     res.status(200).json({ token });
 });
